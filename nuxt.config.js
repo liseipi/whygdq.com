93d3282c7d2c26c86ed358b49e1881960e631797
sakura <sakura@qq.com>
2023-03-24 01:45:51 +0800

    generate sitemap

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,17 +26,25 @@ export default {
       lang: 'en'
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'Keywords', name: 'Keywords', content: '武汉仪工、电气、电工、电力、武汉仪工、仪工电气、仪工电力、武汉仪工电气设备有限公司' },
-      { hid: 'description', name: 'description', content: '武汉仪工仪工电气是一家专门为电力系统用户提供全套在线监测解决方案以及运维检修技术服务的股份制科技企业。所提供的产品和服务已经覆盖发电、输电、变电、配电等领域，并逐步发展成为，以智能在线监测系统集成、电气设备运维检修技术服务为主体，以自主品牌产品研发和生产为发展创新的高新技术企业。' },
-      { name: 'format-detection', content: 'telephone=no' }
+      {charset: 'utf-8'},
+      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+      {
+        hid: 'Keywords',
+        name: 'Keywords',
+        content: '武汉仪工、电气、电工、电力、武汉仪工、仪工电气、仪工电力、武汉仪工电气设备有限公司'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: '武汉仪工仪工电气是一家专门为电力系统用户提供全套在线监测解决方案以及运维检修技术服务的股份制科技企业。所提供的产品和服务已经覆盖发电、输电、变电、配电等领域，并逐步发展成为，以智能在线监测系统集成、电气设备运维检修技术服务为主体，以自主品牌产品研发和生产为发展创新的高新技术企业。'
+      },
+      {name: 'format-detection', content: 'telephone=no'}
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
     ],
     script: [
-      { src: 'https://hm.baidu.com/hm.js?f3e1700416cb2f0dac6ae61af626f55c' }
+      {src: 'https://hm.baidu.com/hm.js?f3e1700416cb2f0dac6ae61af626f55c'}
     ]
   },
 
@@ -59,11 +67,11 @@ export default {
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-  ],
+  buildModules: [],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
+    '@nuxtjs/sitemap'
   ],
 
   // Define the dist directory for your Nuxt application
@@ -76,13 +84,27 @@ export default {
     extractCSS: true,
     cssSourceMap: false,
     filenames: {
-      app: ({ isDev }) => isDev ? '[name].js' : '[name].js',
-      chunk: ({ isDev }) => isDev ? '[name].js' : '[name].js',
-      css: ({ isDev }) => isDev ? '[name].css' : '[name].css',
-      json: ({ isDev }) => isDev ? '[name].json' : '[name].json',
-      img: ({ isDev }) => isDev ? '[path][name].[ext]' : 'img/[name].[ext]',
-      font: ({ isDev }) => isDev ? '[path][name].[ext]' : 'fonts/[name].[ext]',
-      video: ({ isDev }) => isDev ? '[path][name].[ext]' : 'videos/[name].[ext]'
+      app: ({isDev}) => isDev ? '[name].js' : '[name].js',
+      chunk: ({isDev}) => isDev ? '[name].js' : '[name].js',
+      css: ({isDev}) => isDev ? '[name].css' : '[name].css',
+      json: ({isDev}) => isDev ? '[name].json' : '[name].json',
+      img: ({isDev}) => isDev ? '[path][name].[ext]' : 'img/[name].[ext]',
+      font: ({isDev}) => isDev ? '[path][name].[ext]' : 'fonts/[name].[ext]',
+      video: ({isDev}) => isDev ? '[path][name].[ext]' : 'videos/[name].[ext]'
     },
+  },
+  sitemap: {
+    path: '/sitemap.xml',
+    hostname: 'https://www.whygdq.com',
+    generate: false,// 允许使用nuxt generate生成
+    cacheTime: 1000 * 60 * 60 * 24, //1天更新频率，只在generate: false有用
+    gzip: true, // 生成.xml.gz压缩的sitemap
+    defaults: {
+      changefreq: 'always',
+      lastmod: new Date()
+    },
+    exclude: [
+      '/404',
+    ]
   }
 }
